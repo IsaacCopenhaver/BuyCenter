9636c96365a2f7659754008d4a62adf87782b815
Hai-Mie Nguyen <haimienguyen01@gmail.com>
2026-08-26 11:36:21 -0400

    Add Card model

---
+++ server/models/Card.js
@@ -0,0 +1,60 @@
+import { Model, DataTypes } from 'sequelize'
+import sequelize from '../db/database.js'
+
+class Card extends Model {}
+
+Card.init(
+    {
+        shopifyProductId: {
+            type: DataTypes.STRING,
+            primaryKey: true,
+        },
+
+        setId: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        number: {
+            type: DataTypes.STRING,
+        },
+
+        rarity: {
+            type: DataTypes.STRING,
+        },
+
+        tcgProductId: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+
+        gradingId: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+
+        sellPrice: {
+            type: DataTypes.JSONB,
+        },
+
+        buyPrice: {
+            type: DataTypes.JSONB,
+        },
+
+        marketPrice: {
+            type: DataTypes.JSON,
+        },
+    },
+    {
+        sequelize,
+        modelName: 'Card',
+        tableName: 'cards',
+    }
+)
+
+export default Card
